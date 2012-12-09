200266c0d919325e242bb3fc0ed7e4e1e17d361f
unknown <joshhays@MININT-HERDLQ9.ant.amazon.com>
2012-12-09 14:47:14 -0800

    Added favicon.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -29,7 +29,6 @@ app.configure('development', function(){
 
 //app.get('/', routes.index);
 //app.get('/users', user.list);
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
