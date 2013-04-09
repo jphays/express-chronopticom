14b212128bc274e329767a05d4a8425b4d03ac0c
Josh Hays <jphays@gmail.com>
2013-04-08 22:16:17 -0700

    Updated routes

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,10 @@ function init(app)
     app.get('/text', function(req, res) {
         res.render('text', { title: 'Nexus' });
     });
+
+    app.get('/test', function(req, res) {
+        res.render('test')
+    });
 }
 
 module.exports.init = init
