a214b74f613bd2e42a4cf07a952b5fe86e75c66f
Josh Hays <jphays@gmail.com>
2014-05-13 16:06:40 -0700

    Updated express to 4.2.0.

--- config/environment.js
+++ config/environment.js
@@ -2,28 +2,30 @@ var path = require('path'),
     stylus = require('stylus'),
     nib = require('nib');
 
+var favicon = require('serve-favicon'),
+    static = require('serve-static'),
+    logger = require('morgan');
+
 function configure(app, express, root)
 {
-    app.configure(function()
-    {
-        app.set('port', process.env.PORT || 3000);
-        app.set('views', path.join(root, 'views'));
-        app.set('view engine', 'jade');
-        app.use(express.static(path.join(root, 'public')));
-        app.use(express.favicon(path.join(root, 'public', 'images', 'favicon.ico')));
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(stylus.middleware({
-            src: path.join(root, 'views'),
-            dest: path.join(root, 'public'),
-            compile: stylusCompile }));
-    });
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(root, 'views'));
+    app.set('view engine', 'jade');
+    app.use(logger('dev'));
+    app.use(favicon(path.join(root, 'public', 'images', 'favicon.ico')));
+    app.use(static(path.join(root, 'public')));
+    // app.use(express.bodyParser());
+    // app.use(express.methodOverride());
+    // app.use(app.router);
+    // app.use(stylus.middleware({
+    //     src: path.join(root, 'views'),
+    //     dest: path.join(root, 'public'),
+    //     compile: stylusCompile }));
 
-    app.configure('development', function(){
-        app.use(express.errorHandler());
-    });
+    var env = process.env.NODE_ENV || 'development';
+    if (env == 'development') {
+        // app.use(express.errorHandler());
+    };
 };
 
 function stylusCompile(str, path)
