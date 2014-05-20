acc7bc5f5e2fa82e8a5fcb1f77e983b68f0965ec
Josh Hays <jphays@gmail.com>
2014-05-19 21:53:50 -0700

    Polyhedra: some adjustments.

--- public/scripts/polyhedra.js
+++ public/scripts/polyhedra.js
@@ -12,7 +12,7 @@
         var scene, camera, renderer;
         var reflectionCamera;
         var objects = [];
-        var lights = [];
+        var planets = [];
 
         init();
 
@@ -50,51 +50,20 @@
         function initScene()
         {
             scene = new THREE.Scene({
-                fog: THREE.FogExp2(getRandomColor(), 50)
+                // fog: THREE.FogExp2(getRandomColor(), 50)
             });
 
             camera = new THREE.PerspectiveCamera(60, dimensions.width / dimensions.height, 0.1, 1000);
             camera.position.z = 5;
-        }
 
-        function initObjects()
-        {
             reflectionCamera = new THREE.CubeCamera(0.1, 5000, 512);
             //reflectionCamera.renderTarget.mapping = new THREE.CubeRefractionMapping();
             //reflectionCamera.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
             scene.add(reflectionCamera);
+        }
 
-            var geometries = [
-                //new THREE.BoxGeometry(2,2,2),
-                new THREE.DodecahedronGeometry(2, 0),
-                //new THREE.DodecahedronGeometry(2.2, 1),
-                //new THREE.IcosahedronGeometry(2, 0),
-                new THREE.IcosahedronGeometry(2.2, 2),
-                //new THREE.OctahedronGeometry(2, 0),
-                //new THREE.OctahedronGeometry(2.2, 1),
-                //new THREE.PlaneGeometry(100, 100)
-                //new THREE.TorusKnotGeometry(3,1, 48, 48, 2, 3);
-            ];
-
-            //var material = new THREE.MeshNormalMaterial();
-            var material = new THREE.MeshPhongMaterial({
-                color: 0xDDDDEE,
-                specular: 0x333328,
-                shading: THREE.FlatShading,
-                side: THREE.DoubleSide,
-                shininess: 50,
-                transparent: true,
-                opacity: 0.7,
-                refractionRatio: 1.6, //2.4,
-                reflectivity: 0.3,
-                envMap: reflectionCamera.renderTarget,
-            });
-
-            for (var i = 0; i < geometries.length; i++) {
-                var mesh = new THREE.Mesh(geometries[i], material);
-                objects.push(mesh);
-                scene.add(mesh);
-            }
+        function initObjects()
+        {
 
             var floor = new THREE.Mesh(
                 new THREE.PlaneGeometry(1000, 1000),
@@ -103,23 +72,30 @@
             floor.position.y = -9;
             scene.add(floor);
 
-            // $.each(geometries, function(geometry) {
-            //     scene.add(new THREE.Mesh(geometry, material));
-            // });
+            createSun();
+            createPlanets(7);
+
         }
 
         function initLights()
         {
-            // var directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
-            // directionalLight.position.set(0, 0, -3);
+            // var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+            // directionalLight.position.set(0, 5, 0);
+            // directionalLight.lookAt(0, 0, 0);
             // scene.add(directionalLight);
 
-            var areaLight = new THREE.AreaLight( 0xffffff, 1 );
-            areaLight.position.set( 0, 5, -3 );
-            areaLight.rotation.set( 1.5, 0, 0 );
-            areaLight.width = 10;
-            areaLight.height = 10;
-            scene.add(areaLight);
+            // var spotLight = new THREE.SpotLight(0xffffff, 0.8);
+            // spotLight.position.set(0, 10, 0);
+            // spotLight.castShadow = true;
+            // spotLight.lookAt(0, 0, 0);
+            // scene.add(spotLight);
+
+            // var areaLight = new THREE.AreaLight( 0xffffff, 1 );
+            // areaLight.position.set( 0, 5, -3 );
+            // areaLight.rotation.set( 1, 0, 0 );
+            // areaLight.width = 10;
+            // areaLight.height = 10;
+            // scene.add(areaLight);
 
             var areaBulb = new THREE.Mesh(
                 new THREE.PlaneGeometry(10, 10),
@@ -128,19 +104,104 @@
             areaBulb.rotation.set(1, 0, 0 );
             scene.add(areaBulb);
 
-            for (var i = 0; i < 7; i++)
+            var ambientLight = new THREE.AmbientLight(0x202020);
+            scene.add(ambientLight);
+
+        }
+
+        function createSun()
+        {
+
+            var insideSize = Math.random() * 0.5 + 1.5; // 2
+            var outideSize = Math.random() * 0.5 + 2;   // 2.2
+
+            var insideGeometries = [
+                // new THREE.BoxGeometry(insideSize, insideSize, insideSize),
+                // new THREE.TetrahedronGeometry(insideSize, 0),
+                // new THREE.OctahedronGeometry(insideSize, 0),
+                new THREE.DodecahedronGeometry(insideSize, 0),
+                new THREE.IcosahedronGeometry(insideSize, 0),
+            ];
+
+            var outsideGeometries = [
+                //new THREE.DodecahedronGeometry(outideSize, 1),
+                new THREE.IcosahedronGeometry(outideSize, 1),
+                new THREE.IcosahedronGeometry(outideSize, 2)
+            ];
+
+            var geometries = [
+                _.sample(insideGeometries),
+                _.sample(outsideGeometries)
+            ];
+
+            var insideColor = getRandomColor(ColorCollections.lightColors);
+            var materials = [
+                new THREE.MeshPhongMaterial({
+                    color: insideColor,
+                    //specular: getRandomColor(),
+                    shading: THREE.FlatShading,
+                    //side: THREE.DoubleSide,
+                    shininess: 50,
+                    transparent: true,
+                    opacity: 0.8
+                }),
+                new THREE.MeshPhongMaterial({
+                    color: 0xDDDDEE,
+                    specular: 0x333328,
+                    shading: THREE.FlatShading,
+                    side: THREE.DoubleSide,
+                    shininess: 50,
+                    transparent: true,
+                    opacity: 0.7,
+                    refractionRatio: 1.6, //2.4,
+                    reflectivity: 0.3,
+                    envMap: reflectionCamera.renderTarget,
+                })
+            ];
+
+            // var insideLight = new THREE.PointLight(insideColor, 1, 100);
+            // scene.add(insideLight);
+
+            // var directionalLight = new THREE.DirectionalLight(insideColor, 0.8);
+            // directionalLight.position.set(0, 2, 0);
+            // directionalLight.lookAt(0, 0, 0);
+            // scene.add(directionalLight);
+
+            for (var i = 0; i < geometries.length; i++) {
+                var mesh = new THREE.Mesh(geometries[i], materials[i]);
+                objects.push(mesh);
+                scene.add(mesh);
+            }
+
+        }
+
+        function createPlanets(count)
+        {
+
+            for (var i = 0; i < count; i++)
             {
                 var lightColor = getRandomColor(ColorCollections.lightColors);
 
+                // x: (Math.pow(Math.random(), 2)) * 9 - 3,
+                // y: (Math.pow(Math.random(), 1)) * 6 - 2,
+                // z: (Math.pow(Math.random(), 2)) * 9 - 3
+
+                var randomx = Math.random() * 4 - 2;
+                var randomy = Math.random() * 4 - 2;
+                var randomz = Math.random() * 4 - 2;
                 var position = {
-                    x: (Math.pow(Math.random(), 2)) * 9 - 3,
-                    y: (Math.pow(Math.random(), 1)) * 6 - 2,
-                    z: (Math.pow(Math.random(), 2)) * 9 - 3
+                    x: Math.pow(randomx, 2) * sign(randomx) + 2 *sign(randomx),
+                    y: randomy + 1,
+                    z: Math.pow(randomz, 2) * sign(randomz) + sign(randomz)
                 };
 
                 var size = Math.random();
                 var lightbulb = new THREE.Mesh(
-                    new THREE.IcosahedronGeometry(size * 0.3 + 0.05, Math.floor(size * 1.3)),
+                    size < 0.2 ?
+                        new THREE.OctahedronGeometry(size * 0.3 + 0.1) :
+                    size < 0.5 ?
+                        new THREE.DodecahedronGeometry(size * 0.3 + 0.05) :
+                        new THREE.IcosahedronGeometry(size * 0.3 + 0, Math.floor(size * 1.3)),
                     new THREE.MeshPhongMaterial({
                         color: lightColor,
                         shininess: 12,
@@ -156,19 +217,18 @@
                 lightbulb.position.set(position.x, position.y, position.z);
                 pointLight.position.set(position.x, position.y, position.z);
 
-                lights.push({
+                planets.push({
                     light: pointLight,
                     bulb: lightbulb
-                })
+                });
 
                 scene.add(lightbulb);
                 scene.add(pointLight);
             }
 
-            var ambientLight = new THREE.AmbientLight(0x202020);
-            scene.add(ambientLight);
-
         }
+
+        function sign(x) { return x > 0 ? 1 : x < 0 ? -1 : 0; }
 
         function render(time)
         {
@@ -187,9 +247,9 @@
                 objects[i].rotation.z += 0.009;
             }
 
-            for (var i = 0; i < lights.length; i++) {
-                lights[i].bulb.rotation.x -= 0.007;
-                lights[i].bulb.rotation.z -= 0.013;
+            for (var i = 0; i < planets.length; i++) {
+                planets[i].bulb.rotation.x -= 0.007;
+                planets[i].bulb.rotation.z -= 0.013;
             }
 
             camera.position.x = Math.cos( time / 2000 ) * 7; // + (Math.sin(time / 2000) * 1.1);
@@ -208,8 +268,8 @@
         function getRandomColor(colorSet)
         {
             if (!colorSet) colorSet = ColorCollections.tehcolors;
-            var index = Math.floor(Math.random() * colorSet.length);
-            var hexString = colorSet[index].hexString;
+            var color = _.sample(colorSet);
+            var hexString = color.hexString;
             return parseInt(hexString.substr(1), 16);
         }
 
